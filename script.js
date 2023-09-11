4509031b5332add5157cf1cd415e147fb61a13f9
ChenZiQiang <ziqiang.chen@outlook.com>
2023-09-11 23:02:43 +0900

    Update form styles

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
     if (e.target["password"] !== e.target["confirm_password"]) {
-        console.error("password difference");
+        alert("Passwords do not match");
         e.preventDefault();
     }
 });
